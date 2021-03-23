156ef7c7542459a7c31046acbfb1435e4f37139e
Mozhgan <mahloo@amazon.com>
2021-03-23 13:23:08 +0100

    removing last four commits

--- app/scripts/controllers/claims.js
+++ app/scripts/controllers/claims.js
@@ -7,20 +7,10 @@
  * # ClaimsCtrl
  * Controller of the clientApp
  */
-import { listClaimBootcamps } from './graphql/queries';
-
 angular.module('clientApp').controller('ClaimsCtrl', function ($scope, $http, Constants) {
-  
-  //const apiData = await API.graphql({ query: listClaimBootcamps});
-  //const claimsFromAPI = apiData.data.listClaimBootcamps.items;
-  //$scope.claims = claimsFromAPI;
-//    var response = await API.graphql(graphqlOperation(listRestaurants))
-  //  this.restaurants = (response as any).data.listRestaurants.items;
-
   $http.get(Constants.CLAIM_MANAGER_URL + '/claims')
     .then(function(response) {
       $scope.claims = response.data;
-      //$scope.filteredEmployees = $filter('filter')(response.data, {details.name: selectedName }); 
     })
     .catch(function(response) {
       console.error('Error getting claims', response.status, response.data);
